a3dd569967c80c00480cc1c1d45addb1d11a19c9
solarillion <solarillion.ta@gmail.com>
2019-10-17 21:54:54 +0530

    Removed start_ngrok.sh

--- start_ngrok.sh
+++
@@ -1,2 +0,0 @@
-# Author: Nanda H Krishna (https://github.com/nandahkrishna)
-/home/sfserver/ngrok start --all -config=/home/sfserver/config/ngrok.yml --log=stdout > /home/sfserver/.ngrok2/ngrok.log &
