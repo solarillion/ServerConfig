9bd5bcc8475a7442c10542b19d6d0759ccddcfbf
solarillion <solarillion.server@gmail.com>
2019-12-08 03:24:24 +0530

    Added reminder for office hours

--- post_office_hours.py
+++ post_office_hours.py
@@ -6,6 +6,4 @@ tars_token = keys["slack"]
 tars_id = keys["tars"]
 tars = slack.WebClient(token=tars_token)
 
-tars.chat_postMessage(channel=tars_id, text="Wake up!")
-time.sleep(10)
 tars.chat_postMessage(channel=tars_id, text="Post office hours.")
--- remind_office_hours.py
+++ remind_office_hours.py
@@ -6,4 +6,4 @@ tars_token = keys["slack"]
 tars_id = keys["tars"]
 tars = slack.WebClient(token=tars_token)
 
-tars.chat_postMessage(channel=tars_id, text="Request office hours.")
+tars.chat_postMessage(channel=tars_id, text="Remind office hours.")
