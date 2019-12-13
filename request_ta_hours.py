51d0b42ba55b664b5c4c6cce8d68ac573a678264
nandahkrishna <nanda.harishankar@gmail.com>
2019-12-13 12:33:15 +0530

    TA hours updates

--- request_ta_hours.py
+++ request_ta_hours.py
@@ -12,13 +12,13 @@ today = datetime.date.today()
 monday = next_weekday(today, 0)
 sunday = next_weekday(monday, 6)
 
-tars_token = keys["slack"]
+tars_token = keys["sfserver"]
 tars_id = keys["tars"]
 tars = slack.WebClient(token=tars_token)
 
 sf_ta = keys["sf_ta"]
-tars.chat_postMessage(channel=sf_ta, text="Week " + monday.strftime("%d-%m-%Y") + " through " + sunday.strftime("%d-%m-%Y") + ":")
-tars.chat_postMessage(channel=sf_ta, text="<@" + tars_id + "> poll \"Mon-Thu TA Hours\" \"Monday 18:00-20:00\" \"Tuesday 18:00-20:00\" \"Wednesday 18:00-20:00\" \"Thursday 18:00-20:00\"")
-tars.chat_postMessage("Mark your hours by 18:00 on Sunday for Monday through Thursday.")
-tars.chat_postMessage(channel=sf_ta, text="<@" + tars_id + "> poll \"Fri-Sun TA Hours\" \"Friday 18:00-20:00\" \"Saturday 13:00-15:00\" \"Saturday 16:00-18:00\" \"Saturday 18:00-20:00\" \"Sunday 10:30-13:00\" \"Sunday 13:30-16:00\" \"Sunday 16:30-19:00\"")
-tars.chat_postMessage("Mark your hours by 18:00 on Thursday for Friday through Sunday.")
+tars.chat_postMessage(channel=sf_ta, text="Week " + monday.strftime("%d-%m-%Y") + " through " + sunday.strftime("%d-%m-%Y") + ":", as_user=True)
+tars.chat_postMessage(channel=sf_ta, text="<@" + tars_id + "> poll \"Mon-Thu TA Hours\" \"Monday 18:00-20:00\" \"Tuesday 18:00-20:00\" \"Wednesday 18:00-20:00\" \"Thursday 18:00-20:00\"", as_user=True)
+tars.chat_postMessage(channel=sf_ta, text="Mark your hours by 18:00 on Sunday for Monday through Thursday.", as_user=True)
+tars.chat_postMessage(channel=sf_ta, text="<@" + tars_id + "> poll \"Fri-Sun TA Hours\" \"Friday 18:00-20:00\" \"Saturday 13:00-15:00\" \"Saturday 16:00-18:00\" \"Saturday 18:00-20:00\" \"Sunday 10:30-13:00\" \"Sunday 13:30-16:00\" \"Sunday 16:30-19:00\"", as_user=True)
+tars.chat_postMessage(channel=sf_ta, text="Mark your hours by 18:00 on Thursday for Friday through Sunday.", as_user=True)
