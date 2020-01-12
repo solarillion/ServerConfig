f3fb9a8def55c5ae9eb2c95a0927fcb4ce384db8
solarillion <solarillion.server@gmail.com>
2020-01-12 23:26:21 +0530

    Updates

---
+++ track_orientees.py
@@ -0,0 +1,8 @@
+import slack
+from credentials.keys import *
+
+tars_token = keys["slack"]
+tars_id = keys["tars"]
+tars = slack.WebClient(token=tars_token)
+
+tars.chat_postMessage(channel=tars_id, text="Track all orientees sf_ta.")
